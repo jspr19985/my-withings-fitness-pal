6413bb880de2dc1be441a19282562786895deffd
jspr <joannasprott@hotmail.com>
2022-12-22 22:22:35 +0000

    pass in data

---
+++ src/data/temp-data/data.js
@@ -0,0 +1,50 @@
+export const stepsData = [
+    { x: 1, y: 2 },
+    { x: 2, y: 3 },
+    { x: 3, y: 5 },
+    { x: 4, y: 4 },
+    { x: 5, y: 1 }
+];
+export const weightData = [
+    { x: 1, y: 9 },
+    { x: 2, y: 5 },
+    { x: 3, y: 5 },
+    { x: 4, y: 1 },
+    { x: 5, y: 7 }
+];
+
+export const calorieData = [
+    { x: 1, y: 9 },
+    { x: 2, y: 5 },
+    { x: 3, y: 5 },
+    { x: 4, y: 1 },
+    { x: 5, y: 7 }
+];
+
+// needs to be sorted better by area
+const data2012 = [
+    { quarter: 1, earnings: 13000 },
+    { quarter: 2, earnings: 16500 },
+    { quarter: 3, earnings: 14250 },
+    { quarter: 4, earnings: 20000 }
+];
+
+const data2013 = [
+    { quarter: 1, earnings: 15000 },
+    { quarter: 2, earnings: 12500 },
+    { quarter: 3, earnings: 19500 },
+    { quarter: 4, earnings: 13000 }
+];
+
+const data2014 = [
+    { quarter: 1, earnings: 11500 },
+    { quarter: 2, earnings: 13250 },
+    { quarter: 3, earnings: 20000 },
+    { quarter: 4, earnings: 15500 }
+];
+
+export const barData = {
+    data2012,
+    data2013,
+    data2014
+}
